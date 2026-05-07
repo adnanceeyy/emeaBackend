7eac5baa0496c6b06300dc4ee580bd3c33ccada4
adnan <adnancholayil123@gmail.com>
2026-05-07 15:08:13 +0530

    Add health check route to verify live status

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+app.get('/', (req, res) => {
+  res.send('EMEA HSS API is running...');
+});
+
 // Routes
 const authRoutes = require('./routes/auth');
 const studentRoutes = require('./routes/students');
